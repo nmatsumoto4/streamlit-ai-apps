715b52936b5d924410b543ae6ebafe24bdaf561e
nariyuki.matsumoto <nmatsumoto4@gmail.com>
2023-09-30 02:00:28 +0900

    [feat] add pdf qa

--- 00_my_first_app.py
+++ 00_my_first_app.py
@@ -1,132 +1,161 @@
+# pip install pycryptodome
+from glob import glob
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage
-)
+from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Qdrant
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+import chromadb
+# qdrantはなぜか動かないのでChromaDBで代用
+# from qdrant_client import QdrantClient
+# from qdrant_client.models import Distance, VectorParams
+
+CHROMADB_PATH = "./local_chromadb"
+COLLECTION_NAME = "my_collection_2"
+
 
 def init_page():
     st.set_page_config(
-        page_title="Website Summarizer",
+        page_title="Ask My PDF(s)",
         page_icon="🤗"
     )
-    st.header("Website Summarizer 🤗")
-    st.sidebar.title("Options")
-
-
-def init_messages():
-    clear_button = st.sidebar.button("Clear Conversation", key="clear")
-    if clear_button or "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant.")
-        ]
-        st.session_state.costs = []
+    st.sidebar.title("Nav")
+    st.session_state.costs = []
 
 
 def select_model():
-    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-3.5-16k", "GPT-4"))
     if model == "GPT-3.5":
-        model_name = "gpt-3.5-turbo"
+        st.session_state.model_name = "gpt-3.5-turbo"
+    elif model == "GPT-3.5":
+        st.session_state.model_name = "gpt-3.5-turbo-16k"
     else:
-        model_name = "gpt-4"
-
-    return ChatOpenAI(temperature=0, model_name=model_name)
-
-
-def get_url_input():
-    url = st.text_input("URL: ", key="input")
-    return url
+        st.session_state.model_name = "gpt-4"
+    
+    # 300: 本文以外の指示のトークン数 (以下同じ)
+    st.session_state.max_token = OpenAI.modelname_to_contextsize(st.session_state.model_name) - 300
+    return ChatOpenAI(temperature=0, model_name=st.session_state.model_name)
 
 
-def validate_url(url):
-    try:
-        result = urlparse(url)
-        return all([result.scheme, result.netloc])
-    except ValueError:
-        return False
-
-
-def get_content(url):
-    try:
-        with st.spinner("Fetching Content ..."):
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # fetch text from main (change the below code to filter page)
-            if soup.main:
-                return soup.main.get_text()
-            elif soup.article:
-                return soup.article.get_text()
-            else:
-                return soup.body.get_text()
-    except:
-        st.write('something wrong')
+def get_pdf_text():
+    uploaded_file = st.file_uploader(
+        label='Upload your PDF here😇',
+        type='pdf'
+    )
+    if uploaded_file:
+        pdf_reader = PdfReader(uploaded_file)
+        text = '\n\n'.join([page.extract_text() for page in pdf_reader.pages])
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            model_name="text-embedding-ada-002",
+            # 適切な chunk size は質問対象のPDFによって変わるため調整が必要
+            # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
+            # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
+            chunk_size=500,
+            chunk_overlap=0,
+        )
+        return text_splitter.split_text(text)
+    else:
         return None
 
 
-def build_prompt(content, n_chars=300):
-    return f"""以下はとある。Webページのコンテンツである。内容を{n_chars}程度でわかりやすく要約してください。
+def load_store():
+    
+    client = chromadb.PersistentClient(path=CHROMADB_PATH)
 
-========
-
-{content[:1000]}
-
-========
-
-日本語で書いてね！
-"""
+    return Chroma(
+       collection_name="langchain_store",
+        embedding_function=OpenAIEmbeddings(),
+        client=client,
+    )
 
 
-def get_answer(llm, messages):
+def build_vector_store(pdf_text):
+    store = load_store()
+    store.add_texts(pdf_text)
+
+
+def build_qa_model(llm):
+    store = load_store()
+    retriever = store.as_retriever(
+        # "mmr",  "similarity_score_threshold" などもある
+        search_type="similarity",
+        # 文書を何個取得するか (default: 4)
+        search_kwargs={"k":10}
+    )
+    return RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff", 
+        retriever=retriever,
+        return_source_documents=True,
+        verbose=True
+    )
+
+
+def page_pdf_upload_and_build_vector_db():
+    st.title("PDF Upload")
+    container = st.container()
+    with container:
+        pdf_text = get_pdf_text()
+        if pdf_text:
+            with st.spinner("Loading PDF ..."):
+                build_vector_store(pdf_text)
+
+
+def ask(qa, query):
     with get_openai_callback() as cb:
-        answer = llm(messages)
-    return answer.content, cb.total_cost
+        # query / result / source_documents
+        answer = qa(query)
+
+    return answer, cb.total_cost
+
+
+def page_ask_my_pdf():
+    st.title("Ask My PDF(s)")
+
+    llm = select_model()
+    container = st.container()
+    response_container = st.container()
+
+    with container:
+        query = st.text_input("Query: ", key="input")
+        if not query:
+            answer = None
+        else:
+            qa = build_qa_model(llm)
+            if qa:
+                with st.spinner("ChatGPT is typing ..."):
+                    answer, cost = ask(qa, query)
+                st.session_state.costs.append(cost)
+            else:
+                answer = None
+
+        if answer:
+            with response_container:
+                st.markdown("## Answer")
+                st.write(answer)
 
 
 def main():
     init_page()
 
-    llm = select_model()
-    init_messages()
-
-    container = st.container()
-    response_container = st.container()
-
-    with container:
-        url = get_url_input()
-        is_valid_url = validate_url(url)
-        if not is_valid_url:
-            st.write('Please input valid url')
-            answer = None
-        else:
-            content = get_content(url)
-            if content:
-                prompt = build_prompt(content)
-                st.session_state.messages.append(HumanMessage(content=prompt))
-                with st.spinner("ChatGPT is typing ..."):
-                    answer, cost = get_answer(llm, st.session_state.messages)
-                st.session_state.costs.append(cost)
-            else:
-                answer = None
-
-    if answer:
-        with response_container:
-            st.markdown("## Summary")
-            st.write(answer)
-            st.markdown("---")
-            st.markdown("## Original Text")
-            st.write(content)
+    selection = st.sidebar.radio("Go to", ["PDF Upload", "Ask My PDF(s)"])
+    if selection == "PDF Upload":
+        page_pdf_upload_and_build_vector_db()
+    elif selection == "Ask My PDF(s)":
+        page_ask_my_pdf()
 
     costs = st.session_state.get('costs', [])
     st.sidebar.markdown("## Costs")
     st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
     for cost in costs:
         st.sidebar.markdown(f"- ${cost:.5f}")
+
 
 if __name__ == '__main__':
     main()
